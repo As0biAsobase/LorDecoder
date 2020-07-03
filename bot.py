a9af23771abe5d415586a4742f0eff20d7da1933
Gleb Rysev <glebrysev@gmail.com>
2020-07-03 23:11:03 +0300

    added bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+import vk_api
+import requests
+from vk_api.longpoll import VkLongPoll, VkEventType
+from datetime import datetime
+from main import generate_image
+
+base_time = datetime(1970,1,1)
+
+vk_session = vk_api.VkApi(token='574b15b58c2a8c86474fe862c09f38d8b7826d4b3f4df70e48bd72fc7272b73100ab0eb7e962169334834')
+
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+        generate_image(event.text)
+
+        server = vk.photos.getMessagesUploadServer()
+        b = requests.post(server['upload_url'], files={'photo': open('output/output.png', 'rb')}).json()
+        params = {'photo': b['photo'], 'server': b['server'], 'hash': b['hash']}
+        c = vk.photos.saveMessagesPhoto(**params)[0]
+        d = "photo{}_{}".format(c["owner_id"], c["id"])
+
+        vk.messages.send( #Отправляем сообщение
+            user_id=event.user_id,
+            message='test', random_id=(datetime.utcnow()-base_time).total_seconds(),
+            attachment = d)
+   #Слушаем longpoll, если пришло сообщение то:
+        if event.text == 'test' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
+            if event.from_user: #Если написали в ЛС
+                vk.messages.send( #Отправляем сообщение
+                    user_id=event.user_id,
+                    message='test', random_id=(datetime.utcnow()-base_time).total_seconds())
+            elif event.from_chat: #Если написали в Беседе
+                vk.messages.send( #Отправляем собщение
+                    chat_id=event.chat_id,
+                    message='Ваш текст')
--- main.py
+++ main.py
@@ -6,124 +6,120 @@ from io import BytesIO
 import PIL.ImageDraw as ImageDraw
 import PIL.ImageFont as ImageFont
 
-champions = []
-followers = []
-spells = []
 
-champions_total = 0
-followers_total = 0
-spells_total = 0
-
-cost_font = ImageFont.truetype("fonts/Roboto-Black.ttf", 50)
-name_font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 37)
-title_font = ImageFont.truetype("fonts/AmaticSC-Bold.ttf", 70)
-
-jdata = json.loads(open("cards.json",  encoding='utf-8').read())
 
 # Decoding
-deck = LoRDeck.from_deckcode('CEBAIAIFAEHSQNQIAEAQGDAUDAQSOKJUAIAQCBI5AEAQCFYA')
+def generate_image(code):
+    champions = []
+    followers = []
+    spells = []
 
-# list all cards with card format 3:01SI001
-# list(deck)
+    champions_total = 0
+    followers_total = 0
+    spells_total = 0
 
-# natum = Image.open("Natum_Perdere_HD.png")
-# natum.putalpha(200)
-# background.paste(natum)
-# background.show()
-# bg_w, bg_h = background.size
-for each in deck:
-    for dict in jdata:
-        q, code = each.split(':')
-        q = int(q)
-        if dict["cardCode"] == code:
+    cost_font = ImageFont.truetype("fonts/Roboto-Black.ttf", 50)
+    name_font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 37)
+    title_font = ImageFont.truetype("fonts/AmaticSC-Bold.ttf", 70)
+
+    jdata = json.loads(open("cards.json",  encoding='utf-8').read())
+
+    deck = LoRDeck.from_deckcode(code)
+
+    for each in deck:
+        for dict in jdata:
+            q, code = each.split(':')
+            q = int(q)
+            if dict["cardCode"] == code:
             # print(dict["name"])
-            if dict["type"] == "Боец":
-                if dict["supertype"] == "Чемпион":
+                if dict["type"] == "Боец":
+                    if dict["supertype"] == "Чемпион":
                     # background.paste(img, (50, 100 + len(champions)*110))
-                    champions.append({"name" : dict["name"], "cost" : dict["cost"], "cardCode" :  dict["cardCode"], "regionRef" :  dict["regionRef"], "quantity" :  q})
-                    champions_total += q
-                else:
+                        champions.append({"name" : dict["name"], "cost" : dict["cost"], "cardCode" :  dict["cardCode"], "regionRef" :  dict["regionRef"], "quantity" :  q})
+                        champions_total += q
+                    else:
                     # background.paste(img, (550, 100 + len(followers)*110))
-                    followers.append({"name" : dict["name"], "cost" : dict["cost"], "cardCode" :  dict["cardCode"], "regionRef" :  dict["regionRef"], "quantity" :  q})
-                    followers_total += q
-            else:
-                # background.paste(img, (1050, 100 + len(followers)*110))
-                spells.append({"name" : dict["name"], "cost" : dict["cost"], "cardCode" :  dict["cardCode"], "regionRef" :  dict["regionRef"], "quantity" :  q})
-                spells_total += q
+                        followers.append({"name" : dict["name"], "cost" : dict["cost"], "cardCode" :  dict["cardCode"], "regionRef" :  dict["regionRef"], "quantity" :  q})
+                        followers_total += q
+                else:
+                    # background.paste(img, (1050, 100 + len(followers)*110))
+                    spells.append({"name" : dict["name"], "cost" : dict["cost"], "cardCode" :  dict["cardCode"], "regionRef" :  dict["regionRef"], "quantity" :  q})
+                    spells_total += q
 
-height = max([len(champions), len(followers), len(spells)]) * 72 + 200
-background = Image.new('RGBA', (1920, height), (30, 30, 30, 255))
+    height = max([len(champions), len(followers), len(spells)]) * 72 + 200
+    background = Image.new('RGBA', (1920, height), (30, 30, 30, 255))
 
-logo = Image.open("logos/Natum_Perdere_Logo.png")
-logo = logo.resize((640, 360))
-background.paste(logo, (0, 360), mask = logo)
-print(height)
+    logo = Image.open("logos/Natum_Perdere_Logo.png")
+    logo = logo.resize((640, 360))
+    background.paste(logo, (0, height-360), mask = logo)
 
-champions = sorted(champions, key = lambda i: i['cost'])
-followers = sorted(followers, key = lambda i: i['cost'])
-spells = sorted(spells, key = lambda i: i['cost'])
+    champions = sorted(champions, key = lambda i: i['cost'])
+    followers = sorted(followers, key = lambda i: i['cost'])
+    spells = sorted(spells, key = lambda i: i['cost'])
 
-i = 0
-for each in champions:
-    img = Image.open("processed/" + each["cardCode"] + ".png")
-    img = img.resize((600, 70))
-    draw = ImageDraw.Draw(img)
+    i = 0
+    for each in champions:
+        img = Image.open("processed/" + each["cardCode"] + ".png")
+        img = img.resize((600, 70))
+        draw = ImageDraw.Draw(img)
 
-    text = str(each["quantity"])
-    x, y = 570, 10
+        text = str(each["quantity"])
+        x, y = 570, 10
 
-    draw.text((x-2, y-2), text, (0,0,0), font=name_font)
-    draw.text((x+2, y-2), text,(0,0,0),font=name_font)
-    draw.text((x+2, y+2), text, (0,0,0), font=name_font)
-    draw.text((x-2, y+2), text, (0,0,0),font=name_font)
-    draw.text((570, 10), text, font=name_font, fill='rgb(255, 255, 255)')
+        draw.text((x-2, y-2), text, (0,0,0), font=name_font)
+        draw.text((x+2, y-2), text,(0,0,0),font=name_font)
+        draw.text((x+2, y+2), text, (0,0,0), font=name_font)
+        draw.text((x-2, y+2), text, (0,0,0),font=name_font)
+        draw.text((570, 10), text, font=name_font, fill='rgb(255, 255, 255)')
 
-    background.paste(img, (30, 100 + i*72))
-    i += 1
+        background.paste(img, (30, 100 + i*72))
+        i += 1
 
-i = 0
-for each in followers:
-    img = Image.open("processed/" + each["cardCode"] + ".png")
-    img = img.resize((600, 70))
-    draw = ImageDraw.Draw(img)
+    i = 0
+    for each in followers:
+        img = Image.open("processed/" + each["cardCode"] + ".png")
+        img = img.resize((600, 70))
+        draw = ImageDraw.Draw(img)
 
-    text = str(each["quantity"])
-    x, y = 570, 10
+        text = str(each["quantity"])
+        x, y = 570, 10
 
-    draw.text((x-2, y-2), text, (0,0,0), font=name_font)
-    draw.text((x+2, y-2), text,(0,0,0),font=name_font)
-    draw.text((x+2, y+2), text, (0,0,0), font=name_font)
-    draw.text((x-2, y+2), text, (0,0,0),font=name_font)
+        draw.text((x-2, y-2), text, (0,0,0), font=name_font)
+        draw.text((x+2, y-2), text,(0,0,0),font=name_font)
+        draw.text((x+2, y+2), text, (0,0,0), font=name_font)
+        draw.text((x-2, y+2), text, (0,0,0),font=name_font)
 
-    draw.text((x, y), text, font=name_font, fill='rgb(255, 255, 255)')
+        draw.text((x, y), text, font=name_font, fill='rgb(255, 255, 255)')
 
-    background.paste(img, (660, 100 + i*72))
-    i += 1
+        background.paste(img, (660, 100 + i*72))
+        i += 1
 
-i = 0
-for each in spells:
-    img = Image.open("processed/" + each["cardCode"] + ".png")
-    img = img.resize((600, 70))
-    draw = ImageDraw.Draw(img)
+    i = 0
+    for each in spells:
+        img = Image.open("processed/" + each["cardCode"] + ".png")
+        img = img.resize((600, 70))
+        draw = ImageDraw.Draw(img)
 
-    text = str(each["quantity"])
-    x, y = 570, 10
+        text = str(each["quantity"])
+        x, y = 570, 10
 
-    draw.text((x-2, y-2), text, (0,0,0), font=name_font)
-    draw.text((x+2, y-2), text,(0,0,0),font=name_font)
-    draw.text((x+2, y+2), text, (0,0,0), font=name_font)
-    draw.text((x-2, y+2), text, (0,0,0),font=name_font)
+        draw.text((x-2, y-2), text, (0,0,0), font=name_font)
+        draw.text((x+2, y-2), text,(0,0,0),font=name_font)
+        draw.text((x+2, y+2), text, (0,0,0), font=name_font)
+        draw.text((x-2, y+2), text, (0,0,0),font=name_font)
 
-    draw.text((x, y), text, font=name_font, fill='rgb(255, 255, 255)')
+        draw.text((x, y), text, font=name_font, fill='rgb(255, 255, 255)')
 
-    background.paste(img, (1290, 100 + i*72))
-    i += 1
+        background.paste(img, (1290, 100 + i*72))
+        i += 1
 
 
-draw = ImageDraw.Draw(background)
-draw.text((30, 10), "Главари: " + str(champions_total), font=title_font, fill='rgb(255, 255, 255)')
-draw.text((660, 10), "Воены: " + str(followers_total), font=title_font, fill='rgb(255, 255, 255)')
-draw.text((1290, 10), "Колдунства: " + str(spells_total), font=title_font, fill='rgb(255, 255, 255)')
+    draw = ImageDraw.Draw(background)
+    draw.text((30, 10), "Главари: " + str(champions_total), font=title_font, fill='rgb(255, 255, 255)')
+    draw.text((660, 10), "Воены: " + str(followers_total), font=title_font, fill='rgb(255, 255, 255)')
+    draw.text((1290, 10), "Колдунства: " + str(spells_total), font=title_font, fill='rgb(255, 255, 255)')
 
-background.show()
-print("Чемпион: " + str(len(champions)) + " Боец: " + str(len(followers)) + " Заклинание: " + str(len(spells)))
+    background.save("output/output.png")
+
+# generate_image("CEBAIAIDDYVC4LYEAEBAECAJHECAGAIDBMNSYAICAMEQEAICBQSQCAQCAUBACAIDCMAQCARR")
+# print("Чемпион: " + str(len(champions)) + " Боец: " + str(len(followers)) + " Заклинание: " + str(len(spells)))
