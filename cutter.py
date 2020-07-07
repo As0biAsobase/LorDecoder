a85c9a68cca53fc9330584dfa8313e1da5e6a435
Gleb Rysev <glebrysev@gmail.com>
2020-07-07 22:06:31 +0300

    Recut cards, improved visuals

--- cutter.py
+++ cutter.py
@@ -3,10 +3,11 @@ import json
 import PIL.ImageDraw as ImageDraw
 import PIL.ImageFont as ImageFont
 
-cost_font = ImageFont.truetype("fonts/Roboto-Black.ttf", 30)
-name_font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 35)
+cost_font = ImageFont.truetype("fonts/Roboto-Black.ttf", 28)
+name_font = ImageFont.truetype("fonts/YanoneKaffeesatz-Medium.ttf", 38)
+outline_font = ImageFont.truetype("fonts/YanoneKaffeesatz-Bold.ttf", 35)
 
-jdata = json.loads(open("ru_ru/card_data.json",  encoding='utf-8').read())
+jdata = json.loads(open("card_data.json",  encoding='utf-8').read())
 result = []
 i=1
 for dict in jdata:
@@ -19,9 +20,9 @@ for dict in jdata:
         img = Image.open("ru_ru/img/cards/" + dict["cardCode"] + ".png")
         w, h = img.size
         img = img.crop((100, 175, w-150, h - 675))
-        img = img.resize((400, 70))
+        img = img.resize((350, 70))
 
-        background.paste(img, (200,0))
+        background.paste(img, (250,0))
 
         gradient = Image.open("gradients/big/"+ dict["regionRef"] +".png")
         gradient = gradient.resize((600, 70))
@@ -31,15 +32,16 @@ for dict in jdata:
         background.paste(Image.new('RGBA', (35, 56), (66, 114, 245, 255)), (7, 7))
 
         draw = ImageDraw.Draw(background)
-
-        draw.text((15, 20), str(dict["cost"]), font=cost_font, fill='rgb(255, 255, 255)')
-
+        if (dict["cost"] >= 10):
+            draw.text((8, 20), str(dict["cost"]), font=cost_font, fill='rgb(255, 255, 255)')
+        else:
+            draw.text((16, 20), str(dict["cost"]), font=cost_font, fill='rgb(255, 255, 255)')
         x, y = 50, 25
         text = dict["name"]
-        draw.text((x-2, y-2), text, (0,0,0), font=name_font)
-        draw.text((x+2, y-2), text,(0,0,0),font=name_font)
-        draw.text((x+2, y+2), text, (0,0,0), font=name_font)
-        draw.text((x-2, y+2), text, (0,0,0),font=name_font)
+        draw.text((x, y-2), text, (0,0,0), font=name_font)
+        draw.text((x, y+2), text,(0,0,0),font=name_font)
+        draw.text((x-2, y), text, (0,0,0), font=name_font)
+        draw.text((x+2, y), text, (0,0,0),font=name_font)
         draw.text((x, y), text, font=name_font, fill='rgb(255, 255, 255)')
 
         background = background.crop((0, 0, 600, 70))
