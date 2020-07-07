eb8a38c00f651f0a6f172294d1ca5fdb588b09ec
Gleb Rysev <glebrysev@gmail.com>
2020-07-07 18:07:30 +0300

    SHould work in group chats now

--- bot.py
+++ bot.py
@@ -3,10 +3,12 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import vk_api
 import requests
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from datetime import datetime
 from main import generate_image
 import time
+import re
+import traceback
 
 allowed_code_list = ["код:", "код" ,"code", "code:"]
 
@@ -17,22 +19,29 @@ base_time = datetime(1970,1,1)
 load_dotenv(find_dotenv())
 
 vk_session = vk_api.VkApi(token=os.getenv("VKAPI_KEY"))
-longpoll = VkLongPoll(vk_session)
+longpoll = VkBotLongPoll(vk_session, "196727308")
 vk = vk_session.get_api()
 while True:
     try:
         for event in longpoll.listen():
             print(event)
-            if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-                user_get=vk.users.get(user_ids = (event.user_id))
+            if event.type == VkBotEventType.MESSAGE_NEW and event.message.text:
+                # print(event.message.text)
+
+                message_text = re.sub('\[.*\],?\s?','',event.message.text)
+                print(message_text)
+
+                peer_id = event.message["peer_id"]
+                user_get=vk.users.get(user_ids = (event.message.from_id))
                 user_get=user_get[0]
                 first_name=user_get['first_name']
 
                 try:
-                    command, parameter = event.text.split(" ")
+                    command, parameter = message_text.split(" ")
+                    # print(parameter)
                     if command.lower() in allowed_code_list:
                         try:
-                            generate_image(event.text, user_get["id"])
+                            generate_image(parameter, user_get["id"])
 
                             server = vk.photos.getMessagesUploadServer()
                             b = requests.post(server['upload_url'], files={'photo': open('output/output.png', 'rb')}).json()
@@ -40,24 +49,15 @@ while True:
                             c = vk.photos.saveMessagesPhoto(**params)[0]
                             d = "photo{}_{}".format(c["owner_id"], c["id"])
 
-                            vk.messages.send(user_id=event.user_id,
+                            vk.messages.send(peer_id=peer_id,
                                 message='Ты ' + first_name + "? Ну раз " + first_name + ", то держи картинку", random_id=(datetime.utcnow()-base_time).total_seconds(), attachment = d)
                         except TypeError:
-                            vk.messages.send(user_id=event.user_id,
+                            vk.messages.send(peer_id=peer_id,
                                 message='Блип-блоп, глупый бот не пониимет код', random_id=(datetime.utcnow()-base_time).total_seconds())
                 except ValueError:
-                    vk.messages.send(user_id=event.user_id,
-                    message='Блип-блоп, не вижу код', random_id=(datetime.utcnow()-base_time).total_seconds())
+                    traceback.print_exc()
+                     # print(message_text.split(" ")[0])
+                     # pass
     except requests.exceptions.ReadTimeout:
         print("\n Переподключение к серверам ВК \n")
         time.sleep(3)
-   #Слушаем longpoll, если пришло сообщение то:
-        # if event.text == 'test' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
-        #     if event.from_user: #Если написали в ЛС
-        #         vk.messages.send( #Отправляем сообщение
-        #             user_id=event.user_id,
-        #             message='test', random_id=(datetime.utcnow()-base_time).total_seconds())
-        #     elif event.from_chat: #Если написали в Беседе
-        #         vk.messages.send( #Отправляем собщение
-        #             chat_id=event.chat_id,
-        #             message='Ваш текст')
