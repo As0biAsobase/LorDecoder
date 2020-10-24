86e741b14fddc782bc7e51448dbf35293074af53
Gleb Rysev <glebrysev@gmail.com>
2020-10-24 17:03:13 +0100

    Gitignore and database

---
+++ database.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+import os#
+
+load_dotenv(find_dotenv())
+client = MongoClient(os.getenv("MONGODB_KEY"))
+
+result = client['natum-perdere']['cardsCollection'].find({})
+
+print(result)
