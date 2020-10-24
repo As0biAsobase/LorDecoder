b396508987a65dce7997a55a5f509b18eb98678f
Gleb Rysev <glebrysev@gmail.com>
2020-10-24 17:20:36 +0100

    Update database.py

--- database.py
+++ database.py
@@ -6,10 +6,10 @@ import re
 class DBConnection:
     def __init__(self):
         load_dotenv(find_dotenv())
-        client = MongoClient(os.getenv("MONGODB_KEY"))
+        self.client = MongoClient(os.getenv("MONGODB_KEY"))
 
     def searchCard(self, name):
-        result = client['natum-perdere']['cardsCollection'].find({ "name" : re.compile(username, re.IGNORECASE)})
+        result = self.client['natum-perdere']['cardsCollection'].find({ "name" : re.compile(name, re.IGNORECASE)})
         print(list(result))
 
 connection = DBConnection()
