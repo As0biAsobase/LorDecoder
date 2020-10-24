4c9ae34289ac4f1ac304e692adee25bb7b0ad335
Gleb Rysev <glebrysev@gmail.com>
2020-10-24 17:14:24 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 import os
+import re
 
 class DBConnection:
     def __init__(self):
@@ -8,7 +9,7 @@ class DBConnection:
         client = MongoClient(os.getenv("MONGODB_KEY"))
 
     def searchCard(self, name):
-        result = client['natum-perdere']['cardsCollection'].find({ "name" : {$regex : new RegExp(name, "i")}})
+        result = client['natum-perdere']['cardsCollection'].find({ "name" : re.compile(username, re.IGNORECASE)})
         print(list(result))
 
 connection = DBConnection()
