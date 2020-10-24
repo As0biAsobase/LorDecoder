11e93e3c61302b1d722b422189af52fa93bdbd58
Gleb Rysev <glebrysev@gmail.com>
2020-10-24 17:35:01 +0100

    Create meta_stats.py

---
+++ meta_stats.py
@@ -0,0 +1,57 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+import os
+
+def query_archetype_wr(input_cards):
+    # input_cards = ['01DE039', '01DE026', '02DE010', '02BW008']
+    load_dotenv(find_dotenv())
+    client = MongoClient(os.getenv("MONGODB_KEY"))
+    result = client['natum-perdere']['meta-stats'].aggregate([
+        {
+            '$match': {
+                'decklist': {
+                    '$all': input_cards
+                }
+            }
+        }, {
+            '$sort': {
+                'matchesCollected': -1
+            }
+        }, {
+            '$addFields': {
+                'winrate': {
+                    '$divide': [
+                        '$matchesWin', '$matchesCollected'
+                    ]
+                }
+            }
+        }, {
+            '$group': {
+                '_id': None,
+                'count': {
+                    '$sum': 1
+                    },
+                'total_collected': {
+                    '$sum': '$matchesCollected'
+                },
+                'total_won': {
+                    '$sum': '$matchesWin'
+                }
+                }
+        }, {
+            '$project': {
+                '_id' : 0,
+                'total_won': '$total_won',
+                'total_collected': "$total_collected",
+                'average_winrate': {
+                    '$divide': [
+                        '$total_won', '$total_collected'
+                    ]
+                }
+            }
+        }
+    ])
+
+    result = list(result)
+    print(result)
+    return result
