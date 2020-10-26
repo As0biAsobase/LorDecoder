d52961e8780cc329fddb7a169a0a40154a2928e1
Gleb Rysev <glebrysev@gmail.com>
2020-10-26 20:31:43 +0000

    Levenshtein for cardsearch

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
+from Levenshtein import distance
 import os
 import re
 
@@ -12,9 +13,25 @@ class DBConnection:
 
     def searchCard(self, name, cost, attack, health):
 
-        result = self.client['natum-perdere']['cardsCollection'].find({ "name" : re.compile('([a-zA-Z]{0,3}' + name + '[a-zA-Z]{0,3})', re.IGNORECASE)})
+        result = self.client['natum-perdere']['cardsCollection'].find({ "name" : re.compile(name, re.IGNORECASE)})
 
         result = list(result)
+
+        # if we didn't find perfect match and enough information is given attempt to guess (possibly needs optimization)
+        if len(result) == 0 and len(name) >= 3:
+            result = self.client['natum-perdere']['cardsCollection'].find({})
+            result = list(result)
+
+            min_distance = sys.maxsize
+            for each in result:
+                dif = distance(each["name"], name)
+                if dif == 1:
+                    result = each
+                    break
+                elif dif < min_distance:
+                    min_distance = dif
+                    result = each
+        
         result = result[0]
 
         return result
