a1004f68b2367c34e6b84df5b24ba5cd8d6df248
Gleb Rysev <glebrysev@gmail.com>
2021-07-12 17:02:32 +0100

    Update poster.py

--- poster.py
+++ poster.py
@@ -150,7 +150,7 @@ def count_popularity(matches, player_ids):
 
     champion_pie = fig.add_subplot(212)
     champion_pie.set_title("Популярность чемпионов")
-    champion_pie.pie(numbers, labels=labels, startangle=90, colors=colors, counterclock=False, radius=5, textprops={'fontsize': 14})
+    champion_pie.pie(numbers, labels=labels, startangle=90, colors=colors, counterclock=False, radius=5, textprops={'fontsize': 6})
     champion_pie.axis('equal')
     fig.savefig('/home/khun/LorDecoder/output/posting/region_pie.png')
 
